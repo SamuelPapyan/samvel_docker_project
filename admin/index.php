14927e1712b5d28c23bb940d1328138c353da689
SamuelPapyan <samuel.papyan170@gmail.com>
2025-09-30 11:26:08 +0400

    initial commit

---
+++ admin/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ADMIN PANEL</title>
+</head>
+<body>
+    <h1>Admin Page</h1>
+    <p>Hello. It's admin page.</p>
+</body>
+</html>
---
+++ front/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FRONT PANEL</title>
+</head>
+<body>
+    <h1>Front Page</h1>
+    <p>Hello. It's front page.</p>
+</body>
+</html>
