aaeed34757b72b3e2e32320305696028ec783c05
SamuelPapyan <samuel.papyan170@gmail.com>
2025-10-07 07:45:57 +0400

    merge sort and quick sort

--- lab3/index.php
+++ lab3/index.php
@@ -63,5 +63,78 @@
         }
         print_r($arr);
     ?>
+    <h2>Merge Sort</h2>
+    <?php      
+        function mergeSort($array, $n) {
+            if ($n <= 1) return $array;
+            $m = ($n - ($n % 2)) / 2;
+            $n1 = $m;
+            $n2 = $m + ($n % 2);
+            $arr1 = [];
+            $arr2 = [];
+            for ($i = 0; $i < $n1; $i++) {
+                $arr1[$i] = $array[$i];
+            }
+            for ($i = 0; $i < $n2; $i++) {
+                $arr2[$i] = $array[$m + $i];
+            }
+            $arr1 = mergeSort($arr1, $n1);
+            $arr2 = mergeSort($arr2, $n2);
+            $newArr = [];
+            $i = 0; $j = 0;
+            while ($i < $n1 or $j < $n2) {
+                if ($i < $n1 and ($j < $n2 and $arr1[$i] <= $arr2[$j] or $j >= $n2)) {
+                    $newArr[$i + $j] = $arr1[$i];
+                    $i++;
+                }
+                if ($j < $n2 and ($i < $n1 and $arr2[$j] <= $arr1[$i] or $i >= $n1)) {
+                    $newArr[$i + $j] = $arr2[$j];
+                    $j++;
+                }
+            }
+            return $newArr;
+        }
+
+        $arr = [10, 5, 13, 5, 4, 65];
+        print_r($arr);
+        $array = mergeSort($arr, 6);
+        print_r($arr);
+
+    ?>  
+    <h2>Quick Sort</h2>
+    <?php
+        $arr = [42, 51, 6, 4, 14, 6, 1];
+        $n = 7;
+        
+        print_r($arr);
+        
+        function quickSort($array, $n) {
+            if ($n <= 1) return $array;
+            $pivot = $array[$n - 1];
+            $arr1 = [];
+            $arr2 = [];
+            $n1 = 0;
+            $n2 = 0;
+            for ($i = 0; $i < $n-1; $i++) {
+                if ($array[$i] < $pivot) $arr1[$n1++] = $array[$i];
+                else $arr2[$n2++] = $array[$i];
+            }
+            $arr1 = quickSort($arr1, $n1);
+            $arr2 = quickSort($arr2, $n2);
+            $newArr = [];
+            $k = 0;
+            for ($i = 0; $i < $n1; $i++) $newArr[$k++] = $arr1[$i];
+            $newArr[$k++] = $pivot;
+            for ($i = 0; $i < $n2; $i++) $newArr[$k++] = $arr2[$i];
+            return $newArr;
+        }
+        
+        $array = quickSort($arr, $n);
+        print_r($arr);
+    ?>
+    <h2>Shell Sort</h2>
+    <?php
+        
+    ?>
 </body>
 </html>
